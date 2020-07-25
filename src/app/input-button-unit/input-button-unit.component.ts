30d6c00b62c238db4ea0a6e30252a45438557100
Sarah Knowles <sarah06knowles@gmail.com>
2020-07-25 16:09:13 -0500

    Fifth Save making progress

--- src/app/input-button-unit/input-button-unit.component.ts
+++ src/app/input-button-unit/input-button-unit.component.ts
@@ -3,15 +3,11 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-input-button-unit',
   template: `
-    <p>
-      input-button-unit works!
-      The title is: {{ title }}
-    </p>
-
-    <input [value]="title"
+     <input #inputElementRef
+     [value]="title"
     (keyup.enter)="changeTitle($event.target.value)">
 
-    <button (click) ="changeTitle('Button Clicked!')">
+    <button (click) ="changeTitle(inputElementRef.value)">
       Save
     </button>
   `,
